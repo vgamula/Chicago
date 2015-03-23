7b429501b551d71e0d7f7d7677519c9b16267c6d
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-03-24 00:51:16 +0200

    .env bootstrap in tests

--- tests/codeception/bin/_bootstrap.php
+++ tests/codeception/bin/_bootstrap.php
@@ -11,4 +11,7 @@ defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(dirname(dirn
 require(YII_APP_BASE_PATH . '/vendor/autoload.php');
 require(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
 
+Dotenv::load(YII_APP_BASE_PATH);
+Dotenv::required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS','YII_DEBUG']);
+
 Yii::setAlias('@tests', dirname(dirname(__DIR__)));
