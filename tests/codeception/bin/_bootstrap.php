2cc23c18908338db68270ba68f140f33821eb865
Volodymyr Gamula <v.gamula@gmail.com>
2015-03-24 02:06:59 +0200

    I so fucking hate you, travis

--- tests/codeception/bin/_bootstrap.php
+++ tests/codeception/bin/_bootstrap.php
@@ -11,13 +11,9 @@ defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(dirname(dirn
 require(YII_APP_BASE_PATH . '/vendor/autoload.php');
 require(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
 
-Dotenv::load(YII_APP_BASE_PATH);
-Dotenv::required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS','YII_DEBUG']);
-
-if (getenv('TRAVIS')) {
-    putenv('DB_HOST=localhost');
-    putenv('DB_NAME=chicago');
-    putenv('YII_DEBUG=true');
+if (!getenv('TRAVIS')) {
+    Dotenv::load(YII_APP_BASE_PATH);
+    Dotenv::required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS','YII_DEBUG']);
 }
 
 Yii::setAlias('@tests', dirname(dirname(__DIR__)));
