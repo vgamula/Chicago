6c0bc95e0cdfc86d302a99152bf343f1db59e56d
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-03-24 21:13:57 +0200

    config fix

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -1,13 +1,10 @@
 <?php
 
-// Dotenv::load(dirname(dirname(__DIR__)));
-// Dotenv::required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS','YII_DEBUG']);
+Dotenv::load(dirname(dirname(__DIR__)));
+Dotenv::required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS','YII_DEBUG']);
 
-// defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
-// defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
-
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
 
 defined('YII_TEST_ENTRY_URL') or define('YII_TEST_ENTRY_URL', parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PATH));
 defined('YII_TEST_ENTRY_FILE') or define('YII_TEST_ENTRY_FILE', dirname(dirname(__DIR__)) . '/web/index-test.php');
@@ -18,6 +15,6 @@ require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 $_SERVER['SCRIPT_FILENAME'] = YII_TEST_ENTRY_FILE;
 $_SERVER['SCRIPT_NAME'] = YII_TEST_ENTRY_URL;
 $_SERVER['SERVER_NAME'] = parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_HOST);
-$_SERVER['SERVER_PORT'] =  parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PORT) ?: '80';
+$_SERVER['SERVER_PORT'] = parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PORT) ?: '80';
 
 Yii::setAlias('@tests', dirname(__DIR__));
