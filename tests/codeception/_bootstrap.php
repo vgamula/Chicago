1f8c15cc169c8c8d75806313b2e50dc6dc7d0bb3
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-03-24 21:19:24 +0200

    config fix

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+
 Dotenv::load(dirname(dirname(__DIR__)));
 Dotenv::required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS','YII_DEBUG']);
 
@@ -9,8 +12,6 @@ defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
 defined('YII_TEST_ENTRY_URL') or define('YII_TEST_ENTRY_URL', parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PATH));
 defined('YII_TEST_ENTRY_FILE') or define('YII_TEST_ENTRY_FILE', dirname(dirname(__DIR__)) . '/web/index-test.php');
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
-require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 $_SERVER['SCRIPT_FILENAME'] = YII_TEST_ENTRY_FILE;
 $_SERVER['SCRIPT_NAME'] = YII_TEST_ENTRY_URL;
