9cc760e4f98a5741225fb70d60704122a2cbb85f
Volodymyr Gamula <v.gamula@gmail.com>
2015-03-28 17:11:01 +0200

    Added fixtures for project

---
+++ tests/codeception/unit/fixtures/data/projects.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'title' => 'Accusamus voluptatum est est magnam perferendis.',
+        'alias' => 'iste-aut-qui-ea',
+        'shortDescription' => 'Ut dolorum fugit corrupti illum.',
+        'description' => 'Qui dolorem aliquam labore esse sit sit aut dolor. Qui quod sed nisi sunt. Et ut qui ea nihil atque aut. Aspernatur rerum cupiditate et consequuntur. Iure vero repellendus rerum commodi dolorum. In rem et nihil error.',
+        'isPublished' => 1,
+        'rating' => 0,
+        'createdAt' => 1427555388,
+        'updatedAt' => 1427555388,
+    ],
+    [
+        'title' => 'Sint voluptates repellendus exercitationem reiciendis dolorem fugiat odit.',
+        'alias' => 'quaerat-nostrum-nihil-qui-cum-tenetur-voluptatem-culpa',
+        'shortDescription' => 'Dolorum qui iste ut et.',
+        'description' => 'Nam totam et hic optio. Cumque voluptatibus tempore vel est. Perferendis quia vitae doloremque eligendi ut molestiae. Iste quo facilis ipsum ipsum neque. Nihil praesentium tenetur voluptatem vero dolor nam. Quis sint iste nobis amet. Consequatur ipsam et ea ut. Quibusdam sequi dolorem ullam repellat eos ut. Dolores sunt aut necessitatibus.',
+        'isPublished' => 1,
+        'rating' => 0,
+        'createdAt' => 1427555388,
+        'updatedAt' => 1427555388,
+    ],
+];
